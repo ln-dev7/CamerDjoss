bfe33d496c4630e99c80ffca8384575da1525216
LN <62269693+ln-dev7@users.noreply.github.com>
2021-11-19 18:00:48 +0100

    plus de style...

--- scripts/lengthpost.js
+++ scripts/lengthpost.js
@@ -3,4 +3,11 @@ let lenghtNewPost = document.querySelector('.lenght');
 
 newPost.addEventListener('input', () => {
     lenghtNewPost.innerHTML = 500 - parseInt(((newPost.value.split('')).length))
+    if (parseInt(lenghtNewPost.innerHTML) <= 100){
+        lenghtNewPost.style.background = 'red'
+    } else if (parseInt(lenghtNewPost.innerHTML) <= 300){
+        lenghtNewPost.style.background = 'orange'
+    } else{
+        lenghtNewPost.style.background = '#0A72F5'
+    }
 })
